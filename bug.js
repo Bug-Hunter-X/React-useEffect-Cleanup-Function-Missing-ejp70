9baa6d50b0ffc124f16db336e00ede8c6f962d57
BugHunter-X <askzenai@gmail.com>
2025-01-06 07:18:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // Incorrect: Missing return statement for cleanup function
+    console.log('Component mounted');
+    return () => {
+      console.log('Component unmounted');
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+```
